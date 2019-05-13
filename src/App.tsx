3fbf407178a266bae209cca8d6982d8a403ce141
JakubKohoutek <jakub.kohoutek@email.cz>
2019-05-13 22:42:17 +0200

    Fix broken image, update the content

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,17 @@ import './App.css';
 
 class App extends Component {
   render() {
-    const daysToWedding = moment([2019, 8, 21]).diff(moment(), 'days');
+    const daysToWedding = moment([2020, 5, 6]).diff(moment(), 'days');
 
     return (
       <div className="App">
         <header className="App-header">
-          <img src="./public/wedding.png" className="App-logo" alt="Wedding is comming..." />
+          <img src="./wedding.png" className="App-logo" alt="Wedding is comming..." />
           <p>
             Svatba Kuby a Adélky se blíží.
           </p>
-          <p>Zbývá {daysToWedding} dní</p>
+          <p>Bude se konat 6. 6. 2020 v Hotelu Monínec</p>
+          <p>Do dne D zbývá {daysToWedding} dní</p>
         </header>
       </div>
     );
