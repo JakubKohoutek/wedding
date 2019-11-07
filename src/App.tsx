c031be510b2d739fc03783d714006934b0ed15b1
Jakub Kohoutek <jakub.kohoutek@email.cz>
2019-11-07 01:03:33 +0100

    Improve page look

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,16 @@ class App extends Component {
     const daysToWedding = moment([2020, 5, 6]).diff(moment(), 'days');
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src="./wedding.png" className="App-logo" alt="Wedding is comming..." />
-          <p>
-            Svatba Kuby a Adélky se blíží.
-          </p>
-          <p>Bude se konat 6. 6. 2020 v Hotelu Monínec</p>
-          <p>Do dne D zbývá {daysToWedding} dní</p>
+      <div className="app">
+        <header className="app-header">
+          <h1>
+            Svatba Kuby a Adélky se blíží
+          </h1>
         </header>
+        <section className="app-section">
+          <p>Bude se konat 6. 6. 2020 v Hotelu Monínec</p>
+          <p>Do dne D zbývá {daysToWedding} dní!</p>
+        </section>
       </div>
     );
   }
