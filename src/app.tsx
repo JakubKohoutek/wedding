f7812bdfa1cc4a4b3bcdf55c44801b6b5227caf2
Jakub Kohoutek <jakub.kohoutek@email.cz>
2019-11-12 22:39:38 +0100

    Remove lazy loading as not necessary at the moment

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,20 @@
-import React, {Suspense, lazy} from 'react';
+import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Navigation from './components/navigation';
 import Page from './components/page';
 
-const Home = lazy(() => import('./routes/home'));
-const Map = lazy(() => import('./routes/map'));
+import Home from './routes/home';
+import Map from './routes/map';
 
 const App: React.SFC = () => (
   <Router>
     <Page>
       <Navigation />
-      <Suspense fallback={<div>Loading the page...</div>}>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/map" component={Map} />
-        </Switch>
-      </Suspense>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/map" component={Map} />
+      </Switch>
     </Page>
   </Router>
 );
