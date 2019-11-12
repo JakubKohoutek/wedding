ec40e89f62853a8aad8a4912ef04f0fab7b41fcd
Jakub Kohoutek <jakub.kohoutek@email.cz>
2019-11-12 22:39:14 +0100

    Add spinner to map page

---
+++ src/components/spinner/spinner.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './spinner.css';
+
+const Spinner: React.FunctionComponent = () => <div className="spinner" />;
+
+export default Spinner;
--- src/routes/map/map.tsx
+++ src/routes/map/map.tsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import Spinner from '../../components/spinner';
 
 import './map.css';
 
@@ -7,13 +8,11 @@ const Map: React.FunctionComponent = () => {
 
   return (
     <div className="map">
-      {loading && <p>Načítám...</p>}
+      {loading && <Spinner />}
       <iframe
         className={loading ? 'map--loading' : 'map--loaded'}
         title="google-map"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10355.433016035036!2d14.51653045879599!3d49.543827305752565!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470b627942e21671%3A0x382cd11852de2448!2sHotel%20Mon%C3%ADnec!5e0!3m2!1scs!2scz!4v1573323818066!5m2!1scs!2scz"
-        width="600"
-        height="450"
         allowFullScreen
         onLoad={(): void => setLoading(false)}
       />
