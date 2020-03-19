b83a78e07e37be8754268a5708415e8232a93080
Jakub Kohoutek <jakub.kohoutek@merck.com>
2020-03-19 18:18:53 +0100

    Clean up environment variables

--- server/src/processEnv.d.ts
+++ server/src/processEnv.d.ts
@@ -1,12 +1,7 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    PORT: string;
-    JWT_SECRET: string;
-    DB_USER_NAME?: string;
-    DB_USER_PSWD?: string;
-    BASE_API_URL: string;
-    ADMIN_NAME: string;
-    ADMIN_PSWD: string;
-    ADMIN_EMAIL: string;
+    PORT?: string;
+    JWT_SECRET?: string;
+    BASE_API_URL?: string;
   }
 }
